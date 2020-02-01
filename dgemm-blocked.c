dd8df638771c933b741912665c7e4dd1aff69267
will <williamgarner@nevada.unr.edu>
2020-01-31 16:51:28 -0800

    lets see

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -122,7 +122,7 @@ void square_dgemm (int lda, double* A, double* B, double* C)
 		for(int j = 0; j < lda; ++j)
 			for(int k = 0; k < lda; ++k)
 			{
-				C[i] = -1 * A[j] * B[k] -1 >= 0 ? A[j] * B[k] : -1 * A[j] * B[k] -1;
+				C[i] = -1;
 				printf("%d ", C[i]);
 			}
 
