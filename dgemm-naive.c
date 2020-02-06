14faec652d8cced6f0c8833d214ffe6f797cc3c0
William Garner <william.garner7@gmail.com>
2020-02-05 21:37:27 -0800

    kji

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -50,12 +50,16 @@ void square_dgemm (int n, double* A, double* B, double* C)
 
 
   /* For each row i of A */
-  for (int i = 0; i < n; ++i)
+	for( int k = 0; k < n; k++ )
+		for (int j = 0; j < n; ++j)
+			for (int i = 0; i < n; ++i)
+				C[i+j*n] += A[i+k*n] * B[k+j*n];
+
     /* For each column j of B */
-    for (int j = 0; j < n; ++j)
+
     {
       /* Compute C(i,j) */
-      for( int k = 0; k < n; k++ )
-		  C[i+j*n] += A[i+k*n] * B[k+j*n];
+
+
     }
 }
