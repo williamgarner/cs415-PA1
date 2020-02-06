5642731dfd008680ce3576e14b9eaf92ed596ff1
William Garner <william.garner7@gmail.com>
2020-02-05 21:01:53 -0800

    comment out useless code

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -41,14 +41,14 @@ void square_dgemm (int n, double* A, double* B, double* C)
 
 
   /* For each row i of A */
-  for (int i = 0; i < n; ++i)
-    /* For each column j of B */
-    for (int j = 0; j < n; ++j) 
-    {
-      /* Compute C(i,j) */
-      double cij = C[i+j*n];
-      for( int k = 0; k < n; k++ )
-      	cij += A[i+k*n] * B[k+j*n];
-      C[i+j*n] = cij;
-    }
+//  for (int i = 0; i < n; ++i)
+//    /* For each column j of B */
+//    for (int j = 0; j < n; ++j)
+//    {
+//      /* Compute C(i,j) */
+//      double cij = C[i+j*n];
+//      for( int k = 0; k < n; k++ )
+//      	cij += A[i+k*n] * B[k+j*n];
+//      C[i+j*n] = cij;
+//    }
 }
