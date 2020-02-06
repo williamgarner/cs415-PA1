64a90325dcaf2a9e425af722917ea06233d211d8
William Garner <william.garner7@gmail.com>
2020-02-06 10:08:10 -0800

    block outer loop

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -21,12 +21,10 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  * On exit, A and B maintain their input values. */    
 void square_dgemm (int n, double* A, double* B, double* __restrict__ C)
 {
+#pragma block_loop
 	for( int k = 0; k < n; k++ )
-
-		#pragma block_loop
 			for (int j = 0; j < n; ++j)
 			#pragma vector aligned
-			#pragma block_loop
 				for (int i = 0; i < n; ++i)
 					C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
 
