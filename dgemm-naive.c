89f75abc34addf888e5a0b291a17ea538cab2563
William Garner <william.garner7@gmail.com>
2020-02-05 22:14:36 -0800

    maybe be aligned now?

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -23,6 +23,11 @@ void square_dgemm (int n, double* A, double* B, double* __restrict__ C)
 {
 	for (int j = 0; j < n; ++j)
 		for( int k = 0; k < n; k++ )
+		{
+			double cij = C[i+ j*n];
 			for (int i = 0; i < n; ++i)
-				C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
+				cij += A[i+ k*n] * B[k+ j*n];
+			C[i+ j*n] = cij;
+		}
+
 }
