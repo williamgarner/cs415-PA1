7f85380d78d830fb285647f33a49df431c477566
William Garner <william.garner7@gmail.com>
2020-02-06 11:09:25 -0800

    align?

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -18,12 +18,11 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values. */    
-void square_dgemm (int n, double*  A, double* B, double* C)
+void square_dgemm (int n, __align(8) double*  A, double* B, double* C)
 {
 #pragma loop_count min(31), max(769), avg(345)
 #pragma block_loop factor(256) level(1)
 	for (int j = 0; j < n; ++j)
-#pragma loop_count min(31*31), max(769*769), avg(345*345)
 		for( int k = 0; k < n; k++ )
 				for (int i = 0; i < n; ++i)
 					C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
