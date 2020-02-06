0c17761ded69ac2f9cb41a6842b03e108bd077fb
William Garner <william.garner7@gmail.com>
2020-02-06 10:11:27 -0800

    more block?

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -23,6 +23,7 @@ void square_dgemm (int n, double* A, double* B, double* __restrict__ C)
 {
 #pragma block_loop
 	for (int j = 0; j < n; ++j)
+#pragma block_loop
 		for( int k = 0; k < n; k++ )
 			#pragma vector aligned
 				for (int i = 0; i < n; ++i)
