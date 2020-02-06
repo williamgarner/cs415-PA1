e558f43eed65ea98faf3538980da37fde99d99d0
William Garner <william.garner7@gmail.com>
2020-02-06 10:58:47 -0800

    loop count 2

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -23,6 +23,7 @@ void square_dgemm (int n, double*  A, double* B, double* C)
 #pragma loop_count min(31), max(769), avg(345)
 #pragma block_loop factor(256) level(1)
 	for (int j = 0; j < n; ++j)
+#pragma loop_count min(31*31), max(769*769), avg(345*345)
 		for( int k = 0; k < n; k++ )
 				for (int i = 0; i < n; ++i)
 					C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
