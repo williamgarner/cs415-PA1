e390f7dbad7aeefffa615c3dd52273bb7a61c69c
William Garner <william.garner7@gmail.com>
2020-02-06 11:26:41 -0800

    512?

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -21,7 +21,7 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 void square_dgemm (int n, double*  A, double* B, double* C)
 {
 #pragma loop_count min(31), max(769), avg(345)
-#pragma block_loop factor(256) level(1)
+#pragma block_loop factor(512) level(1)
 	for (int j = 0; j < n; ++j)
 		for( int k = 0; k < n; k++ )
 				for (int i = 0; i < n; ++i)
