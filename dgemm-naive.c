dbf6e9bf628b49b172b19b77b148bb552f7be7c5
William Garner <william.garner7@gmail.com>
2020-02-05 21:40:49 -0800

    kij

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -28,38 +28,8 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  * On exit, A and B maintain their input values. */    
 void square_dgemm (int n, double* A, double* B, double* C)
 {
-//	for(int i = 0; i < n; ++i)
-//	{
-//		for(int j = 0; j < n; ++j)
-//		{
-//			for(int k = 0; k < n; ++k)
-//			{
-//				C[j * n + k] += A[j * n + i] * B[j*n + k];
-//			}
-//		}
-//	}
-
-
-//	for (int i = 0; i < n; i++)
-//		for (int k = 0; k < n; k++)
-//			for (int j = 0; j < n; j++)
-//				if (likely(k))
-//					C[i*n + j] += A[i*n + k] * B[k*n + j];
-//				else
-//					C[i*n + j] = A[i*n + k] * B[k*n + j];
-
-
-  /* For each row i of A */
 	for( int k = 0; k < n; k++ )
-		for (int j = 0; j < n; ++j)
-			for (int i = 0; i < n; ++i)
-				C[i+j*n] += A[i+k*n] * B[k+j*n];
-
-    /* For each column j of B */
-
-    {
-      /* Compute C(i,j) */
-
-
-    }
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
 }
