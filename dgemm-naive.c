59cca6c6a0f5622d52c075e3a4b684bee57baee1
William Garner <william.garner7@gmail.com>
2020-02-06 10:44:30 -0800

    just level 1

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -20,7 +20,7 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  * On exit, A and B maintain their input values. */    
 void square_dgemm (int n, double*  A, double* B, double* C)
 {
-#pragma block_loop factor(256) level(1:2)
+#pragma block_loop factor(256) level(1)
 	for (int j = 0; j < n; ++j)
 		for( int k = 0; k < n; k++ )
 				for (int i = 0; i < n; ++i)
