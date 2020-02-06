2b941e16edd5d800e71868a92573ed83132b938e
William Garner <william.garner7@gmail.com>
2020-02-05 22:02:45 -0800

    __restrict

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -19,7 +19,7 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values. */    
-void square_dgemm (int n, double* A, double* B, double restrict* C)
+void square_dgemm (int n, double* A, double* B, double __restrict* C)
 {
 	for (int j = 0; j < n; ++j)
 		for( int k = 0; k < n; k++ )
