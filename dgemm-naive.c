6d2be965f602f191c0d51ed2295787cf192af765
William Garner <william.garner7@gmail.com>
2020-02-06 00:53:49 -0800

    lol

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -27,10 +27,6 @@ void square_dgemm (int n, double* A, double* B, double* __restrict__ C)
 		for( int k = 0; k < n; k++ )
 			#pragma vector aligned
 			for (int i = 0; i < n; ++i)
-			{
-				register double* cij = &C[i+ j*n];
-				register double aik = A[i+ k*n];
-				*cij += aik * B[k+ j*n];
-			}
+				C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
 
 }
