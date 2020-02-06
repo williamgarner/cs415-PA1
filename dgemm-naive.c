36d60498850465fad1e1cc3abeccc8b5a6da3ac3
William Garner <william.garner7@gmail.com>
2020-02-05 22:22:47 -0800

    force align now

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -22,12 +22,8 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 void square_dgemm (int n, double* __attribute__ ((aligned (8)))A, double* B, double* __restrict__ __attribute__ ((aligned (8)))C)
 {
 	for (int j = 0; j < n; ++j)
-		for( int k = 0; k < n; k++ )
-		{
-			double cij = C[i+ j*n];
+		for( int k = 0; k < n; k++ )\
 			for (int i = 0; i < n; ++i)
-				cij += A[i+ k*n] * B[k+ j*n];
-			C[i+ j*n] = cij;
-		}
+				C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
 
 }
