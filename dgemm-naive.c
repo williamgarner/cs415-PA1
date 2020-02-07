5658f6210c994021601264cb707b9fa147e05107
William Garner <william.garner7@gmail.com>
2020-02-06 19:27:30 -0800

    simple?

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -20,7 +20,7 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  * On exit, A and B maintain their input values. */    
 void square_dgemm (int n, double*  A, double* B, double* C)
 {
-	double T[n*n] __attribute__((aligned(sizeof(double))));
+	double T[n*n];
 	for(int i = 0; i < n; ++i)
 		for(int j = 0; j < n; ++j)
 			T[i*n + j] = A[j*n + i];
