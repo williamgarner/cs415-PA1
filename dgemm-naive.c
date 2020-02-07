e5dbcfa4125adff8d6f2faebb7a9be2f2ae5174e
William Garner <william.garner7@gmail.com>
2020-02-06 20:12:25 -0800

    unaligned

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -21,22 +21,22 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
 void square_dgemm (int n, double*  A, double* B, double* C)
 {
 
-	if(n < 191)
-	{
-		#pragma loop_count min(31), max(769), avg(345)
-		#pragma block_loop factor(1024) level(1)
-		for (int j = 0; j < n; ++j)
-			for( int k = 0; k < n; k++ )
-				for (int i = 0; i < n; ++i)
-					C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
-	}
-	else
-	{
+//	if(n < 191)
+//	{
+//		#pragma loop_count min(31), max(769), avg(345)
+//		#pragma block_loop factor(1024) level(1)
+//		for (int j = 0; j < n; ++j)
+//			for( int k = 0; k < n; k++ )
+//				for (int i = 0; i < n; ++i)
+//					C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
+//	}
+//	else
+//	{
 			double T[n*n] __attribute__((aligned(32)));
 			#pragma loop_count min(31), max(769), avg(345)
 			#pragma block_loop factor(1024) level(1)
 				for(int i = 0; i < n; ++i)
-					#pragma vector aligned
+					#pragma vector unaligned
 					for(int j = 0; j < n; ++j)
 							T[i*n + j] = A[j*n + i];
 
@@ -47,5 +47,5 @@ void square_dgemm (int n, double*  A, double* B, double* C)
 					for (int i = 0; i < n; ++i)
 						for( int k = 0; k < n; ++k)
 								C[i+ j*n] += T[k+ i*n] * B[k+ j*n];
-	}
+//	}
 }
