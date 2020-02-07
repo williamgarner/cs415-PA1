8d1584d26227e33129cb90ed857372ebcce88e5c
William Garner <william.garner7@gmail.com>
2020-02-06 20:11:12 -0800

    aligned

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -36,8 +36,9 @@ void square_dgemm (int n, double*  A, double* B, double* C)
 			#pragma loop_count min(31), max(769), avg(345)
 			#pragma block_loop factor(1024) level(1)
 				for(int i = 0; i < n; ++i)
+					#pragma vector aligned
 					for(int j = 0; j < n; ++j)
-						T[i*n + j] = A[j*n + i];
+							T[i*n + j] = A[j*n + i];
 
 
 			#pragma loop_count min(31), max(769), avg(345)
