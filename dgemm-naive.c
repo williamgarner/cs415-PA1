57d4bb129f637368372ddef2cc243c4c0ea700b6
William Garner <william.garner7@gmail.com>
2020-02-09 19:06:39 -0800

    bye bye comments

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -20,35 +20,17 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  * On exit, A and B maintain their input values. */    
 void square_dgemm (const int n, double*  A, double* B, double* restrict C)
 {
-
-//	if(n < 191)
-//	{
-//		#pragma loop_count min(31), max(769), avg(345)
-//		#pragma block_loop factor(1024) level(1)
-//		for (int j = 0; j < n; ++j)
-//			for( int k = 0; k < n; k++ )
-//				#pragma vector unaligned
-//				for (int i = 0; i < n; ++i)
-//					C[i+ j*n] += A[i+ k*n] * B[k+ j*n];
-//	}
-//	else
-//	{
 			double T[n*n];
-//			#pragma loop_count min(31), max(769), avg(345)
-			#pragma block_loop level(2) factor(300)
 				for(int i = 0; i < n; ++i)
 					#pragma vector unaligned
 					for(int j = 0; j < n; ++j)
 							T[i*n + j] = A[j*n + i];
 
 
-//			#pragma loop_count min(31), max(769), avg(345)
-//			#pragma block_loop factor(256)  level(2)
-//				#pragma block_loop level(1:2)
 				for (int j = 0; j < n; ++j)
 					for (int i = 0; i < n; ++i)
 						#pragma vector unaligned
 						for( int k = 0; k < n; ++k)
 								C[i+ j*n] += T[k+ i*n] * B[k+ j*n];
-//	}
+
 }
