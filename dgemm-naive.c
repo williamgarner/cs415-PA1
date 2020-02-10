8fb1290ff0dd0cfe25ea9c282d65cdf200307ed3
William Garner <william.garner7@gmail.com>
2020-02-09 20:59:03 -0800

    jik

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -62,8 +62,8 @@ void square_dgemm (const int n, double*  A, double* B, double* restrict C)
 				T[i*n + j] = A[j*n + i];
 
 
-	for (int i = 0; i < n; ++i)
 	for (int j = 0; j < n; ++j)
+	for (int i = 0; i < n; ++i)
 #pragma vector unaligned
 	for( int k = 0; k < n; ++k)
 
