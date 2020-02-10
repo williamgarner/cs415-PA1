0bd796e85800c508215667c40b1beb29c19604e7
William Garner <william.garner7@gmail.com>
2020-02-09 20:03:32 -0800

    lol what?

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -27,45 +27,45 @@ void square_dgemm (const int n, double*  A, double* B, double* restrict C)
 {
 
 //	printf("SIZE: %d\n", n);
-//			double T[n*n];
-//			for(int blockStart = 0; blockStart < n; blockStart += BLOCK_SIZE)
-//			{
-//				int blockEnd = min(blockStart + BLOCK_SIZE, n);
-//
-//
-////					int temp;
-////					scanf("%d", &temp);
-//				for(int i = blockStart; i < blockEnd; ++i)
-//				{
-//					for(int j = blockStart; j < blockEnd; ++j)
-//					{
-////							printf("%d, %d\n", i, j);
-//
-//						T[i*n + j] = A[j*n + i];
-//					}
-//				}
-//			}
-//
-//
-//
-//				for (int j = 0; j < n; ++j)
-//					for (int i = 0; i < n; ++i)
-//						#pragma vector unaligned
-//						for( int k = 0; k < n; ++k)
-//								C[i+ j*n] += T[k+ i*n] * B[k+ j*n];
+			double T[n*n];
+			for(int blockStart = 0; blockStart < n; blockStart += BLOCK_SIZE)
+			{
+				int blockEnd = min(blockStart + BLOCK_SIZE, n);
 
 
-	double T[n*n];
-	for(int i = 0; i < n; ++i)
-#pragma vector unaligned
-			for(int j = 0; j < n; ++j)
-				T[i*n + j] = A[j*n + i];
+//					int temp;
+//					scanf("%d", &temp);
+				for(int i = blockStart; i < blockEnd; ++i)
+				{
+					for(int j = blockStart; j < blockEnd; ++j)
+					{
+//							printf("%d, %d\n", i, j);
+
+						T[i*n + j] = A[j*n + i];
+					}
+				}
+			}
 
 
-	for (int j = 0; j < n; ++j)
-		for (int i = 0; i < n; ++i)
-#pragma vector unaligned
-				for( int k = 0; k < n; ++k)
-					C[i+ j*n] += T[k+ i*n] * B[k+ j*n];
+
+				for (int j = 0; j < n; ++j)
+					for (int i = 0; i < n; ++i)
+						#pragma vector unaligned
+						for( int k = 0; k < n; ++k)
+								C[i+ j*n] += T[k+ i*n] * B[k+ j*n];
+
+
+//	double T[n*n];
+//	for(int i = 0; i < n; ++i)
+//#pragma vector unaligned
+//			for(int j = 0; j < n; ++j)
+//				T[i*n + j] = A[j*n + i];
+//
+//
+//	for (int j = 0; j < n; ++j)
+//		for (int i = 0; i < n; ++i)
+//#pragma vector unaligned
+//				for( int k = 0; k < n; ++k)
+//					C[i+ j*n] += T[k+ i*n] * B[k+ j*n];
 
 }
